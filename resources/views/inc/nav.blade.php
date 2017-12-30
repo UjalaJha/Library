e82a5f8e9898f05a596c60a9ecf4676185e0c8c2
guest <2015abid.inamdar@ves.ac.in>
2017-12-30 11:50:19 +0530

    changes made

--- resources/views/inc/nav.blade.php
+++ resources/views/inc/nav.blade.php
@@ -6,7 +6,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="index.html">Library</a>
+                <a class="navbar-brand" href="/Library/public/">Library</a>
             </div>
             <!-- /.navbar-header -->
 
