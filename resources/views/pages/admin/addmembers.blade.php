827681c664ad582c9ba2aa58ac604a1f93753a9f
Unknown <anishamulvaidya@gmail.com>
2018-01-02 15:41:34 +0530

    Add members updated

--- resources/views/pages/admin/addmembers.blade.php
+++ resources/views/pages/admin/addmembers.blade.php
@@ -49,12 +49,21 @@
                 <h2>Member</h2>
                 {!! Form::open(['method' => 'POST']) !!}
                 <div class="form-group">
-                    {{Form::label('title', 'Title')}}
-                    {{ Form::text('title', '', ['class' =>'form-control', 'placeholder' => 'Title']) }}
+                    {{Form::label('title', 'Member Name')}}
+                    {{ Form::text('title', '', ['class' =>'form-control', 'placeholder' => 'Add name']) }}
                 </div>
-                <div>
-                    {{Form::label('body', 'Body')}}
-                    {{ Form::textarea('body', '', ['class' =>'form-control', 'placeholder' => 'Enter suggestions']) }}
+                <div class="form-group">
+                    {{Form::label('title', 'Member ID')}}
+                    {{ Form::text('title', '', ['class' =>'form-control', 'placeholder' => 'Add ID']) }}
+                </div>
+                <div class="form-group">
+                    {{Form::label('title', '')}}
+                    {{ Form::text('title', '', ['class' =>'form-control', 'placeholder' => 'Add ID']) }}
+                </div>
+                <div class="form-group dropdown">
+                    {!! Form::label('select', 'Category', ['class' => ' control-label'] )  !!}<br>
+                    {!!  Form::select('select', ['DG' => 'Degree', 'MCA' => 'MCA'],  'S', ['class' => 'form-control dropdown' ]) !!}
+
                 </div>
                 <p></p>
                 {{Form::submit('Submit', ['class' =>'btn btn-primary'])}}
